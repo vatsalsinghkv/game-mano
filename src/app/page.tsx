c4654ea8aaa8850a4c24d863a7d592ced0410610
vatsal <vatsalsinghkv@gmail.com>
2023-10-16 22:29:26 +0530

    feat: icon add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import { ProductSlider, Products } from '@/components/home';
 import { Hero } from '@/components/shared';
 import { Button } from '@/components/ui';
+import { ArrowRightCircle } from 'lucide-react';
 
 export default function Home() {
   return (
     <>
       <Hero title='logo electronics' subtitle='The techies you love'>
-        <Button variant='outline' as='link' href='/products'>
+        <Button
+          variant='outline'
+          as='link'
+          href='/products'
+          className='flex items-center gap-5'
+        >
           View All Products
+          <ArrowRightCircle size={32} />
         </Button>
       </Hero>
       <ProductSlider />
